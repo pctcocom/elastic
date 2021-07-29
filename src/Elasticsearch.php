95c9e8e628b603b9c69703691f431abc9b52f39b
pctcocom <pctco.com@gmail.com>
2021-07-29 21:21:07 +0800

    new zip

---
+++ src/Elasticsearch.php
@@ -0,0 +1,12 @@
+<?php
+namespace Pctco\Elastic\Elasticsearch;
+ /**
+ * @name Elasticsearch
+ * @describe Elasticsearch API
+ * @author https://github.com/pctcocom/elastic
+ **/
+class Elasticsearch{
+   function __construct(){
+      $this->client = [1,2,3,4,5];
+   }
+}
